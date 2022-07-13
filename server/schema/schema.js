3d6598cbcffba30bb8922c7d028f0e8a07bae55f
Israel <tosinmoses384@gmail.com>
2022-07-13 22:56:06 +0100

    feat(getOne):
      - implement get one by id.

---
+++ server/schema/schema.js
@@ -0,0 +1,34 @@
+const { projects, transactions } = require('../sampleData.js')
+
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql');
+
+// Client Type.  The convention is to use uppercase C in ClientType
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        status: { type: GraphQLString },
+        type: { type: GraphQLString },
+
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        client: {
+            type: ClientType,
+            args: {id: { type: GraphQLID }},
+            resolve(parent, args) {
+                return transactions.find(transaction => transaction.id === args.id);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
